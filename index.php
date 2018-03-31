b8fb3c3cb94916a595c6deae4e9ec8760724489e
Sam Kong <samuelwdk@gmail.com>
2018-03-31 20:57:40 +0800

    change menu

--- index.php
+++ index.php
@@ -220,7 +220,7 @@ google.maps.event.addDomListener(window, "load", initialize);
           <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>{{ Auth::user()->name}}</p>
+          <p>Samuel</p>
           <!-- Status -->
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
@@ -243,23 +243,7 @@ google.maps.event.addDomListener(window, "load", initialize);
         <!-- Optionally, you can add icons to the links -->
         <li class="active"><a href="/"><i class="fa fa-link"></i> <span>Dashboard</span></a></li>
         <li><a href="{{ url('employee-management') }}"><i class="fa fa-link"></i> <span>Employee Management</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>System Management</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ url('system-management/department') }}">Department</a></li>
-            <li><a href="{{ url('system-management/division') }}">Division</a></li>
-            <li><a href="{{ url('system-management/country') }}">Country</a></li>
-            <li><a href="{{ url('system-management/state') }}">State</a></li>
-            <li><a href="{{ url('system-management/city') }}">City</a></li>
-            <li><a href="{{ url('system-management/report') }}">Report</a></li>
-          </ul>
-        </li>
-        <li><a href="{{ route('user-management.index') }}"><i class="fa fa-link"></i> <span>User management</span></a></li>
-      </ul>
+
       <!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
