cc19f6cc0548a0d1c45ac0020d6f626459c5ddf5
Sam Kong <samuelwdk@gmail.com>
2018-03-31 19:35:48 +0800

    add header footer2

--- index.php
+++ index.php
@@ -168,14 +168,14 @@ google.maps.event.addDomListener(window, "load", initialize);
                   <!-- Menu Toggle Button -->
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                     <!-- The user image in the navbar-->
-                    <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="user-image" alt="User Image">
+                    <img src="/bower_components/AdminLTE/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
                     <!-- hidden-xs hides the username on small devices so only the image appears. -->
                     <span class="hidden-xs">{{ Auth::user()->username }}</span>
                   </a>
                   <ul class="dropdown-menu">
                     <!-- The user image in the menu -->
                     <li class="user-header">
-                      <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
+                      <img src="/bower_components/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                       <p>
                         Hello {{ Auth::user()->username }}
